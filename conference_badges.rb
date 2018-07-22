b3f779ea3d769b1e067925bf34497ea47d0e9e4d
Gina Lee <ginalee1114@gmail.com>
2018-07-22 13:06:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+# require 'benchmark'
+# require 'bigdecimal/math'
+
+>>>>>>> 413ee032ffa388ba79a4673159548078315e6370
 def badge_maker(name)
   "Hello, my name is #{name}."
 end 
@@ -19,6 +25,7 @@ def assign_rooms(list_of_names)
   list
 end
 
+<<<<<<< HEAD
 def printer(list_of_names)
   batch_badge_creator(list_of_names).each do |badge|
     puts badge
@@ -27,3 +34,20 @@ def printer(list_of_names)
     puts assignment
   end
 end
+=======
+def printer(list)
+  count = 0..list.size-1
+  print_phrases = []
+  for index in count 
+    batch_badge_creator(list).each_with_index do |badge_phrase, index|
+      print_phrases << badge_phrase
+    end 
+    assign_rooms(list).each_with_index do |room_phrase, index|
+      print_phrases << room_phrase
+    end
+  end
+  phrase
+end 
+
+# puts Benchmark.measure{ BigMath.PI(10_0000) }
+>>>>>>> 413ee032ffa388ba79a4673159548078315e6370
